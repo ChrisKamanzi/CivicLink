da33ea59e4cb6c55d23880e24c6c477f181c1edf
user <chrisibambe@gmail.com>
2025-07-29 16:16:46 +0200

    Added error handling on the signUp  and a loading state

--- lib/Providers/RegisterNotifier.dart
+++ lib/Providers/RegisterNotifier.dart
@@ -1,42 +1,60 @@
+import 'dart:io';
+
 import 'package:civic_link/Model/user.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class SignUpNotifier extends StateNotifier<Client?> {
-  SignUpNotifier() : super(null);
+class SignUpNotifier extends StateNotifier<AsyncValue<Client?>> {
+  SignUpNotifier() : super(const AsyncValue.data(null));
 
   final supabase = Supabase.instance.client;
 
-  Future<void> signUp(String email, String password) async {
+  Future<void> signUp(
+    String email,
+    String password, {
+    bool showLogs = false,
+  }) async {
+    state = AsyncValue.loading();
     try {
       final response = await supabase.auth.signUp(
         email: email,
         password: password,
       );
-
       final user = response.user;
-
       if (user != null) {
-        final client = Client(
-          email: email,
-          password: password,
-          userId: user.id,
-        );
+        final client = Client(email: email, password: password, id: user.id);
 
-        state = client;
+        if (showLogs = true) {
+          debugPrint("data sent : ${client}");
+        }
+        state = AsyncValue.data(client);
       }
-    } catch (e) {
-      print("Sign-up error: $e");
-      rethrow;
+    }
+    on SocketException catch(_){
+      final errMsg = ' No internet Connection';
+
+      if (showLogs == true) {
+        debugPrint ('No internet Connection');
+      }
+      state = AsyncValue.error(errMsg, StackTrace.current);
+    }
+    catch (e, st) {
+      if (showLogs = true) debugPrint("Sign-up error: $e");
+      state = AsyncValue.error(e, st);
     }
   }
-
   Future<void> signOut() async {
-    await supabase.auth.signOut();
-    state = null;
+    try {
+      await supabase.auth.signOut();
+      state = AsyncValue.data(null);
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+    }
   }
 }
 
-final registerProvider = StateNotifierProvider<SignUpNotifier, Client?>(
-  (ref) => SignUpNotifier(),
-);
+final registerProvider =
+    StateNotifierProvider<SignUpNotifier, AsyncValue<Client?>>(
+      (ref) => SignUpNotifier(),
+    );
